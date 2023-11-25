b25f30430db83a4b1ddc45ff067fb3e153f023b5
GhiathBr <ghiathbrai46@gmail.com>
2023-11-25 23:46:20 +0100

    first commit

---
+++ 8queen.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+void main(){
+int matrix [8][8]={0};
+int i;
+int j;
+for (i=0;i<8;i++){
+    for(j=0;j<8;j++){
+        printf("%d",matrix[i][j]);
+        printf("\t");
+    }
+    printf("\n");
+}
+
+int ligne,colone;
+int trys=0;
+ do {
+    printf("donner le nombre de ligne:");
+    scanf("%d",&ligne);
+    printf("donner le nombre de colone:");
+    scanf("%d",&colone);
+    
+     for (i=0;i<8;i++){
+         if(matrix[ligne][colone]==8){
+            printf("invalide\n");
+            break;
+        }
+        else {
+          
+        matrix[ligne][i]=8;
+        matrix[i][colone]=8;
+        }
+         if (i==colone ){
+            matrix[ligne][colone]=1;
+        }
+            int i=ligne;
+    int j=colone;
+    while(i>=0 && j<8){
+        if(i!=ligne && j!=colone){
+            matrix[i][j]=8;
+        }
+        j+=1;
+        i-=1;
+    }
+    i=ligne;
+    j=colone;
+    while(i<8 && j>=0){
+        if(i!=ligne && j!=colone){
+            matrix[i][j]=8;
+        }
+        j-=1;
+        i+=1;
+    }
+    i=ligne;
+    j=colone;
+    while(i>=0 && j>=0){
+        if(i!=ligne && j!=colone){
+            matrix[i][j]=8;
+        }
+        j-=1;
+        i-=1;
+    }
+    i=ligne;
+    j=colone;
+    while(i<8 && j<8){
+        if(i!=ligne && j!=colone){
+            matrix[i][j]=8;
+        }
+        j+=1;
+        i+=1;
+    }
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+    for (int i=0;i<8;i++){
+    for(int j=0;j<8;j++){
+        printf("%d",matrix[i][j]);
+        printf("\t");
+    }
+    printf("\n");
+}
+}while(trys<8);
+
+
+}
